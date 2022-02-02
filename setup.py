7966a9dc27377627809538d9c2accc58b4aa6158
korbash <korbash179@gmail.com>
2022-02-02 17:32:48 +0300

    add setap.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+ifrom setuptools import setup, find_packages
+from os.path import join, dirname
+
+setup(
+    name='helloworld',
+    version='1.0',
+    author='korbash and Artem',
+    author_email='korbash179@gmail.com',
+    packages=find_packages('sensor_lib'),
+    long_description=open(join(dirname(__file__), 'README.txt')).read(),
+)
