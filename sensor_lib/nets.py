6844712d69b3bca71ed3334498d28577676e1ce2
RQC-Robotics <95026214+RQC-Robotics@users.noreply.github.com>
2022-02-07 14:07:48 +0300

    Update nets.py

--- sensor_lib/nets.py
+++ sensor_lib/nets.py
@@ -20,3 +20,19 @@ class SensorNN4S(Model):
                                                layers.Reshape(output_shape)])
     def call(self, x):
         return self.sequential(x)
+    
+class SensorNN5S(Model):
+    def __init__(self, input_shape, output_shape):
+        super(SensorNN4S, self).__init__()
+        self.sequential = tf.keras.Sequential([layers.Reshape((input_shape[0], input_shape[1], 1)),
+                                               layers.Conv2D(4, (5, 1), strides=(1, 1), activation='relu', kernel_initializer='random_normal'),
+                                               layers.Conv2D(16, (5, 1), strides=(1, 1), activation='relu', kernel_initializer='random_normal'),
+                                               layers.Conv2D(64*16, (5, input_shape[1]), activation='relu', kernel_initializer='random_normal'),
+                                               layers.Conv2D(64*16, (5, 1), activation='relu', kernel_initializer='random_normal'),
+                                               layers.Flatten(),
+                                               layers.Dense(900, activation='relu', kernel_initializer='random_normal'),
+                                               layers.Dense(30*30, activation='relu'),
+                                               layers.Dense(64*64),
+                                               layers.Reshape(output_shape)])
+    def call(self, x):
+        return self.sequential(x)
