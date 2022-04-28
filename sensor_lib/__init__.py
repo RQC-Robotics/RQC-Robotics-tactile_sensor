9c488f10f86c1a2bb7ebfb5a105260a1b50dff20
korbash <44533769+korbash@users.noreply.github.com>
2022-04-29 00:12:59 +0300

    Update __init__.py

--- sensor_lib/__init__.py
+++ sensor_lib/__init__.py
@@ -1,2 +1,2 @@
 from .nets import *
-from .sim import * # generate_pressure_map, fiber_sim, sim_on_gpu, prepare_dataset_for_train
+from .tf_sim import * # generate_pressure_map, fiber_sim, sim_on_gpu, prepare_dataset_for_train
