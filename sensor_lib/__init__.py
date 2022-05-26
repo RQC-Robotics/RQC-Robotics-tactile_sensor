74eddd8b8b87ff028c87e3bd7cceb0a4b2c543e0
Hacker1337 <amirfvb@gmail.com>
2022-05-26 17:48:58 +0300

    Added first torchnet and notebook for testing

--- sensor_lib/__init__.py
+++ sensor_lib/__init__.py
@@ -1,2 +1,3 @@
-from .nets import *
+from .nets import *     # SensorNN3, SensorNN3B, SensorNN4S, SensorNN5S, SensorNN5S_norm, SensorNN5S_norm2, SensorNN, SensorNN5S_norm_deep
 from .tf_sim import *    # generate_pressure_map, fiber_sim, sim_on_gpu, prepare_dataset_for_train
+from .torch_nets import *
